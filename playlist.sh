6d69fe8e68076e4e02b655d8207acc97a808a8ae
Raxgahrax <jujumercier1991@yahoo.fr>
2020-02-21 20:52:50 +0100

    Update playlist.sh

--- playlist.sh
+++ playlist.sh
@@ -1,38 +1,48 @@
 #! /bin/sh  
 
-#01 Alouette \nGrand Ouest
+#01 Alouette Grand Ouest \n
 mpc add http://alouette.ice.infomaniak.ch/alouette-high.mp3 
-#02 APRR Nord \n(B.F.C.)
+#02 APRR Nord (B.F.C.) \n
 mpc add http://media.autorouteinfo.fr:8000/direct_nord.mp3 
-#03 APRR Sud \n(Rhône Alpes)
+#03 APRR Sud (Rhône Alpes) \n
 mpc add http://media.autorouteinfo.fr:8000/direct_sud.mp3
-#04 Cerise FM \n(Cernay 68)
+#04 BFM Business \n
+mpc add http://chai5she.cdn.dvmr.fr/bfmbusiness
+#05 Cerise FM Cernay 68) \n
 mpc add http://cerisefm.ice.infomaniak.ch/cerisefm-128.mp3
-#05 Cherie FM \n
+#06 Cherie FM \n
 mpc add http://cdn.nrjaudio.fm/audio1/fr/30201/mp3_128.mp3?origine=todae
-#06 Europe Un \n
+#07 Europe Un \n
 mpc add http://mp3lg4.tdf-cdn.com/9240/lag_180945.mp3 
-#07 Fun Radio \n
-mpc add http://streaming.radio.funradio.fr/fun-1-48-192 
-#09 France Culture \n
+#08 Fun Radio \n
+mpc add http://streaming.radio.funradio.fr/fun-1-48-192
+#09 France Bleu Belfort Montbéliard \n
+mpc add http://direct.francebleu.fr/live/fbbelfort-midfi.mp3
+#10 France Bleu Besançon \n
+mpc add http://direct.francebleu.fr/live/fbbesancon-midfi.mp3
+#11 France Culture \n
 mpc add http://chai5she.lb.vip.cdn.dvmr.fr/franceculture-midfi.mp3?ID=ue234gn36t
-#10 France Info \n
+#12 France Info \n
 mpc add http://direct.franceinfo.fr/live/franceinfo-midfi.mp3?ID=ue234gn36t
-#11 France Inter \n
+#13 France Inter \n
 mpc add http://direct.franceinter.fr/live/franceinter-midfi.mp3
-#12 La Grosse Radio \nRock
+#14 La Grosse Radio Rock \n
 mpc add http://ice2.lagrosseradio.info/lagrosseradio-rock-192.mp3
-#13 Nostalgie \n
+#15 Nostalgie \n
 mpc add http://cdn.nrjaudio.fm/audio1/fr/30601/mp3_128.mp3?origine=todae
-#14 NRJ \n
+#16 NRJ \n
 mpc add http://cdn.nrjaudio.fm/audio1/fr/30001/mp3_128.mp3?origine=todae
-#15 Rire & Chansons \nCanulars
-mpc add http://cdn.nrjaudio.fm/adwz1/fr/30413/mp3_128.mp3?origine=todae
-#16 RTL \n
+#17 Rire & Chansons Canulars \n
+mpc add https://scdn.nrjaudio.fm/adwz1/fr/30413/aac_64.mp3
+#18 RTS La 1ere \n
+mpc add http://stream.srg-ssr.ch/m/la-1ere/mp3_128
+#19 RTL \n
 mpc add http://streaming.radio.rtl.fr/rtl-1-48-192  
-#17 RTL 2 \n
+#20 RTL 2 \n
 mpc add http://streaming.radio.rtl2.fr/rtl2-1-48-192  
-#18 Sputnik France \n
+#21 Sputnik France \n
 mpc add https://nfw.ria.ru/flv/audio.aspx?ID=6854085&type=mp3 
-#19 Virgin Radio \n
+#22 Village FM \n
+mpc add http://149.202.186.113/VILLAGESFM
+#23 Virgin Radio \n
 mpc add http://mp3lg4.tdf-cdn.com/9243/lag_164753.mp3
